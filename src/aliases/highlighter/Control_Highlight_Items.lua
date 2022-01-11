802212b12c90015dfebd810e3a889e1fad96cb3d
Damian <demonnic@gmail.com>
2022-01-11 16:44:05 -0500

    Remove default fg of red, and fix some error messages

--- src/aliases/highlighter/Control_Highlight_Items.lua
+++ src/aliases/highlighter/Control_Highlight_Items.lua
@@ -7,14 +7,14 @@ if matches[2] == "add" then
     return
   end
   if not Highlighter.categories[itemTbl[1]] then
-    Highlighter:echo("could not find category {itemTbl[1]} to add {itemTbl[2]} to")
+    Highlighter:echo(f"Could not find category {itemTbl[1]} to add {itemTbl[2]} to")
     return
   end
   Highlighter:add(itemTbl[2], itemTbl[1])
   Highlighter:echo("Added " .. itemTbl[2] .. " to category: " .. itemTbl[1])
 else
   if not Highlighter.items2category[matches[3]] then
-    Highlighter:echo(f"item {matches[3]} not found to remove")
+    Highlighter:echo(f"Item {matches[3]} not found to remove")
   end
   Highlighter:remove(matches[3])
   Highlighter:echo("Removed '" .. matches[3] .. "'" )
